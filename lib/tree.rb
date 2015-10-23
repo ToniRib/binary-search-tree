7d31267eaeda50a02c1f7af4731ec4ac9b504595
ToniRib <tonimarierib@gmail.com>
2015-10-23 10:56:30 -0600

    feat: Adds count & max_height to Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -12,4 +12,24 @@ class Tree
       head.push(node)
     end
   end
+
+  def count
+    head ? head.count : 0
+  end
+
+  def max_height
+    head ? head.max_height : 0
+  end
+end
+
+if __FILE__ == $0
+  numbers = (0..1000).to_a.sample(16)
+  bst = Tree.new
+  numbers.each do |num|
+    bst.insert(num)
+  end
+
+  puts bst.inspect
+  puts bst.count
+  puts bst.max_height
 end
--- spec/tree_test.rb
+++ spec/tree_test.rb
@@ -9,4 +9,24 @@ class TreeTest < Minitest::Test
 
     assert_equal 10, bst.head.data
   end
+
+  def test_adds_smaller_node_to_left_without_overwriting_head_node
+    bst = Tree.new
+    bst.insert(10)
+    bst.insert(8)
+
+    assert_equal 10, bst.head.data
+    assert_equal 8, bst.head.left.data
+    refute bst.head.right
+  end
+
+  def test_adds_larger_node_to_right_without_overwriting_head_node
+    bst = Tree.new
+    bst.insert(10)
+    bst.insert(12)
+
+    assert_equal 10, bst.head.data
+    assert_equal 12, bst.head.right.data
+    refute bst.head.left
+  end
 end
